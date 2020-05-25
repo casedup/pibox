e1c7482e09d942bb31622aa5c36458f0b7a3af2b
casedup <jatavais@trillnetworks.com>
2020-05-25 00:45:57 -0400

    refactored kit A features in shopping into angularjs

--- namesController.js
+++ namesController.js
@@ -2,11 +2,10 @@ var app = angular.module('myApp', [])
 
 	app.controller('namesCtrl', function($scope) {
 	    $scope.names = [
-	    {name:'PiBox Starterkit B'},
 	    {feature:'Raspberry Pi 4'},
 	    {feature:'Mini-HDMI to HDMI Cable'},
 	    {feature:'USB Type-C Power Adapter'},
 	    {feature:'32GB San-disk w/ software installed'},
-	    {feature:'32GB San-disk w/ software installed'},
+	    {feature:'5v Fan + 3 Heat sinks'}
   ];
 });
