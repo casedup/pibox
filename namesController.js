73cc1106c938f3be0ab1567c72571d76daafa5b6
casedup <jatavais@trillnetworks.com>
2020-05-24 23:31:09 -0400

    added namescontroller.js file

---
+++ namesController.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp', [])
+	app.controller('namesController', function($scope) {
+		$scope.modelA = [
+		{name:'PiBox Starter Kit A'}
+		{includes: 'Raspberry Pi 4'},
+		{includes: 'Mini-HDMI to HDMI Cable'},
+		{includes: 'USB Type-C Power Adapter'},
+		{includes: '32GB San-disk w/ software installed'},
+		{includes: '5v Fan + 3 Heat sinks'}
+		
+		]
+	
+	});
