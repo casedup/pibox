20d76bb193249e5029ba57d409d5601d8b5253fa
casedup <jatavais@trillnetworks.com>
2020-05-25 13:22:18 -0400

    added urlImageB to bundle model

--- namesController.js
+++ namesController.js
@@ -3,27 +3,27 @@ var app = angular.module('myApp', [])
 	app.controller('namesCtrl', function($scope) {
 	    $scope.names = [
 	  
-	    {name:'PiBox Starterkit A',feature:'Raspberry Pi 4',price:'$175',imageUrl:'media/img/pi4-trans.jpg'},
-	    {name:'PiBox Starterkit A',feature:'Mini-HDMI to HDMI Cable'},
-	    {name:'PiBox Starterkit A',feature:'USB Type-C Power Adapter'},
-	    {name:'PiBox Starterkit A',feature:'32GB San-disk w/ software installed'},
-	    {name:'PiBox Starterkit A',feature:'5v Fan + 3 Heat sinks'}
+	    {name:'PiBox - Max Starter A',feature:'Raspberry Pi 4',price:'$175',imageUrl:'media/img/pi4-trans.jpg',imageUrlB:'media/img/pi-bundle1.png'},
+	    {name:'PiBox - Max Starter A',feature:'Mini-HDMI to HDMI Cable'},
+	    {name:'PiBox - Max Starter A',feature:'USB Type-C Power Adapter'},
+	    {name:'PiBox - Max Starter A',feature:'32GB San-disk w/ software installed'},
+	    {name:'PiBox - Max Starter A',feature:'5v Fan + 3 Heat sinks'}
   ];
   		$scope.namesB = [
 
-  		 {name:'PiBox Starterkit B',feature:'Starterkit A', price:'$200', imageUrl:'media/img/pi-bundle3a.png'},
-	    {name:'PiBox Starterkit B',feature:'Wired Keyboard + Mouse'},
-	    {name:'PiBox Starterkit B',feature:'Debrid + Trakt Account'},
-	    {name:'PiBox Starterkit B',feature:'Unlimited Youtube API'},
-	    {name:'PiBox Starterkit B',feature:'Tech Support w/ Updates'}
+  		 {name:'PiBox - Max Starter B',feature:'Starterkit A', price:'$200', imageUrl:'media/img/pi-bundle3a.png',imageUrlB:'media/img/pi-bundle.png'},
+	    {name:'PiBox - Max Starter B',feature:'Wired Keyboard + Mouse'},
+	    {name:'PiBox - Max Starter B',feature:'Debrid + Trakt Account'},
+	    {name:'PiBox - Max Starter B',feature:'Unlimited Youtube API'},
+	    {name:'PiBox - Max Starter B',feature:'Tech Support w/ Updates'}
   	];
   		$scope.namesC = [
 
-  		 {name:'PiBox Starterkit C',feature:'Starterkit B', price:'$200', imageUrl:'media/img/pi-bundle4a.jpg'},
-	    {name:'PiBox Starterkit C',feature:'Wireless SNES Remote Controller'},
-	    {name:'PiBox Starterkit C',feature:'128GB Thumbdrive'},
-	    {name:'PiBox Starterkit C',feature:'Dual 5v Fan'},
-	    {name:'PiBox Starterkit C',feature:'100 Free Roms!'}
+  		 {name:'PiBox - Max Starter C',feature:'Starterkit B', price:'$200', imageUrl:'media/img/pi-bundle4a.jpg',imageUrlB:'media/img/pi4-works1.jpg'},
+	    {name:'PiBox - Max Starter C',feature:'Wireless SNES Remote Controller'},
+	    {name:'PiBox - Max Starter C',feature:'128GB Thumbdrive'},
+	    {name:'PiBox - Max Starter C',feature:'Dual 5v Fan'},
+	    {name:'PiBox - Max Starter C',feature:'100 Free Roms!'}
   	];
   	
   	$scope.showCase = [
@@ -32,4 +32,9 @@ var app = angular.module('myApp', [])
   		{name:'Versatile Cheap Powerful',description:'Gaming, Browsing, LIVE TV all supported!',imageUrl:'media/img/gaming-pc.jpeg'}
   	];
   	
+  	$scope.aboutUs = [
+  	
+  	
+  	
+  	]
 });
